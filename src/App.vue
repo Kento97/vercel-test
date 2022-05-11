d16585f2f5c02c6754fd7ea528e41b94ef757928
Human Town <ty.1997@foxmail.com>
2022-05-11 16:50:09 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="成功部署到vercel,SUCCESS!!!" />
+      <HelloWorld msg="成功部署到Vercel, SUCCESS!!!" />
     </div>
   </header>
 
